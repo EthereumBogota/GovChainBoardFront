98fd673192e2904c4c92182fd07b8f73b2e9591f
Joseth Calderon <stevencalcor04@gmail.com>
2023-10-22 12:20:05 +0000

    Adding info to notifications

--- src/views/Notifications.js
+++ src/views/Notifications.js
@@ -90,27 +90,31 @@ function Notifications() {
             <Card>
               <CardHeader>
                 <CardTitle tag="h4">Notifications Style</CardTitle>
+                <p>Here you can see the differents notifications that you have</p>
               </CardHeader>
               <CardBody>
                 <Alert color="info">
-                  <span>This is a plain notification</span>
+                  <span>Enjoy our Dapp and its differents feature😁</span>
                 </Alert>
                 <UncontrolledAlert color="info">
-                  <span>This is a notification with close button.</span>
+                  <span>In dashboard you will find info about differents DAO🔍</span>
+                </UncontrolledAlert>
+                <UncontrolledAlert color="info">
+                  <span>The stats are interactive and totally uploaded📈</span>
+                </UncontrolledAlert>
+                <UncontrolledAlert color="info">
+                  <span>If you have feedback, you can do it in our github🤩</span>
                 </UncontrolledAlert>
                 <UncontrolledAlert className="alert-with-icon" color="info">
                   <span className="tim-icons icon-bell-55" data-notify="icon" />
                   <span data-notify="message">
-                    This is a notification with close button and icon.
+                    Some of the notifications will appear in your screen when you're in other section of the website
                   </span>
                 </UncontrolledAlert>
                 <UncontrolledAlert className="alert-with-icon" color="info">
                   <span className="tim-icons icon-bell-55" data-notify="icon" />
                   <span data-notify="message">
-                    This is a notification with close button and icon and have
-                    many lines. You can see that the icon and the close button
-                    are always vertically aligned. This is a beautiful
-                    notification. So you don't have to worry about the style.
+                    We would like to send you notifications not only in our website, we would like to send you these notifications to your metamask wallet, in this way you will be update to any new in our website
                   </span>
                 </UncontrolledAlert>
               </CardBody>
@@ -120,42 +124,43 @@ function Notifications() {
             <Card>
               <CardHeader>
                 <CardTitle tag="h4">Notification states</CardTitle>
+                <p>The colors will represent several types of notification, in this way you will be able to see how important it can be only seeing the color</p>
               </CardHeader>
               <CardBody>
                 <UncontrolledAlert color="primary">
                   <span>
                     <b>Primary - </b>
-                    This is a regular notification made with ".alert-primary"
+                    This is a normal or basic notification
                   </span>
                 </UncontrolledAlert>
                 <UncontrolledAlert color="info">
                   <span>
                     <b>Info - </b>
-                    This is a regular notification made with ".alert-info"
+                    This notification will give you information about changes or inforvative aspects to our website
                   </span>
                 </UncontrolledAlert>
                 <UncontrolledAlert color="success">
                   <span>
                     <b>Success - </b>
-                    This is a regular notification made with ".alert-success"
+                    This notification will active when you vote in your DAO or you make an specific action in our website
                   </span>
                 </UncontrolledAlert>
                 <UncontrolledAlert color="warning">
                   <span>
                     <b>Warning - </b>
-                    This is a regular notification made with ".alert-warning"
+                    It will be active when you don't vote or when is missing a few minutes to finish the votes
                   </span>
                 </UncontrolledAlert>
                 <UncontrolledAlert color="danger">
                   <span>
                     <b>Danger - </b>
-                    This is a regular notification made with ".alert-danger"
+                    This notification will work when you active an alart about a speacial category you would like to participate or when a specific user vote
                   </span>
                 </UncontrolledAlert>
               </CardBody>
             </Card>
           </Col>
-          <Col md="12">
+      {/* <Col md="12">
             <Card>
               <CardBody>
                 <div className="places-buttons">
@@ -236,7 +241,7 @@ function Notifications() {
                 </div>
               </CardBody>
             </Card>
-          </Col>
+          </Col> */}
         </Row>
       </div>
     </>
